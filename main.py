d06eaf3402f4a9fe1e389e20bcac0207f2ccbc2f
Lucas Zurverra <lzurverra@gmail.com>
2023-09-05 11:31:38 -0300

    added args support

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 from broker import InvertirOnline
 from database import Database
 from supabase_database import SupabaseDatabase
-#from github import Github
 from pathlib import Path
+import argparse
 
 
 class Task:
@@ -21,46 +21,44 @@ class Task:
             print('')
         print('[+] Ejecutando InvertirOnline database backup & restore...')
         
-    def execute(self):
-        #gh = Github()
-        
+    def execute(self, show=True):
         iol = InvertirOnline()
         data = iol.portafolio()
 
-        #db = Database()
-        #db.add_dict_data(data)
         db = SupabaseDatabase()
         db.insert_many(data)
 
-        #gh.upload()
-        
-        self.welcome_message(clear=True)
+        if show:
+            self.welcome_message(clear=True)
         
         return True
 
 
-def plan():
+def main():
+    parser = argparse.ArgumentParser(description='Can do actions automatically')
+
+    parser.add_argument('-n', '--now', action='store_true', help='Gets stocks and stores in the database')
+
+    args = parser.parse_args()
+
     task = Task()
     task.welcome_message()
     
-    task.execute()
+    if args.now:
+        task.execute(False)
+    else:
+        schedule.every().monday.at('18:20').do(task.execute)
+        schedule.every().tuesday.at('18:20').do(task.execute)
+        schedule.every().wednesday.at('18:20').do(task.execute)
+        schedule.every().thursday.at('18:20').do(task.execute)
+        schedule.every().friday.at('18:20').do(task.execute)
 
-def main():
-    task = Task()
-    task.welcome_message()
-
-    schedule.every().monday.at('18:20').do(task.execute)
-    schedule.every().tuesday.at('18:20').do(task.execute)
-    schedule.every().wednesday.at('18:20').do(task.execute)
-    schedule.every().thursday.at('18:20').do(task.execute)
-    schedule.every().friday.at('18:20').do(task.execute)
-
-    try:
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
-    except (KeyboardInterrupt, SystemExit):
-        exit(1)
+        try:
+            while True:
+                schedule.run_pending()
+                time.sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            exit(1)
 
 if __name__ == '__main__':
     main()
